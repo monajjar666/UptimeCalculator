d02687aaf3711288260bd825c8210d53a8fc076d
Mo <ic21b001@technikum-wien.at>
2022-12-17 23:23:36 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/uptimecalculator/UptimeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.uptimecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimeCalculatorApplication.class, args);
+	}
+
+}
